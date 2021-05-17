c501810e5afcda6c0bbe8a7af34894b9a0270d09
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-17 17:26:02 -0400

    write method to print out numbers as many times as the value of the number

---
+++ src/com/qa/Iterators/printTenTimes.java
@@ -0,0 +1,19 @@
+package com.qa.Iterators;
+
+public class printTenTimes {
+	 public static void main(String[] args) {
+//	        for(int i = 0; i <= 10; i++) {
+//	            System.out.println(i);
+//	            
+//	            
+//	        }
+		 method1(10);
+	    }
+	 
+	 public static void method1(Integer input) {
+		for(int i = 0; i < input; i++) {
+			System.out.println(input); 
+//			System.out.println(i);
+		}
+	 }
+}
