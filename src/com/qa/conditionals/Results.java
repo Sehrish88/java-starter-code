32543d579420fa4fe3963abe68a8ff1ca97f8573
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-17 16:01:09 -0400

    create conditionals package

---
+++ src/com/qa/conditionals/Results.java
@@ -0,0 +1,16 @@
+package com.qa.conditionals;
+
+public class Results {
+	
+	int Physics;
+	int Chemistry;
+	int Biology;
+	int total;
+	int percentagel;
+		public static void main() {
+			
+		}
+	}
+	
+	
+
