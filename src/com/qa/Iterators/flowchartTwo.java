5d0c390193ca9e926a372678f978f976af8c4248
Sehrish88 <sehrishbalouch4@gmail.com>
2021-05-17 16:37:49 -0400

    add flowchart classes and display them using java code

---
+++ src/com/qa/Iterators/flowchartTwo.java
@@ -0,0 +1,17 @@
+package com.qa.Iterators;
+
+public class flowchartTwo {
+	
+	
+	public static void main(String[] args) {
+		for(int i = 100; i < 200; i++ ) {
+			if(i % 2 == 0 ) {
+				System.out.println("-");
+			} else {
+				System.out.println("*");
+				
+			}
+		}
+	}
+	
+}
